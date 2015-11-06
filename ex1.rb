c5196e15e9e3078d0af3c918eccbf046e0c3d822
Ludo-Boa <ludo.bolla@gmail.com>
2015-11-06 17:56:44 +0100

    exos 1 a 10

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "hello world"
+puts "hello again"
+puts "I like typing this"
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not'. "
+puts 'I "said" do not touch this.'
---
+++ ex3.rb
@@ -0,0 +1,26 @@
+puts "I will now count my chickens:"
+# le prix du poulet 
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+# le prix de oeufs
+puts "Now I will count the eggs:" 
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# est ce que ceci est vrai
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# pourquoi ceci est faux
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+
+puts "I will now count my train tickets?"
+puts 75.50 + 85.50
